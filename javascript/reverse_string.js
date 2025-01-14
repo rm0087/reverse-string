6ff878ad47f664edd6d94efc6838053bb0131ed5
rm0087 <michetti.ray@gmail.com>
2025-01-13 22:00:13 -0500

    finished js

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,19 +1,32 @@
 function reverseString(str) {
-  // type your code here
+  let arr = str.split('');
+  let i = 0;
+  let x = arr.length - 1;
+
+  while (i < x) {
+    let temp = arr[i];
+    arr[i] = arr[x]
+    arr[x] = temp
+
+    i ++
+    x --
+    
+  }
+  return arr.join('')
 }
+console.log(reverseString("p"))
+// if (require.main === module) {
+//   // add your own tests in here
+//   console.log("Expecting: 'ih'");
+//   console.log("=>", reverseString("hi"));
 
-if (require.main === module) {
-  // add your own tests in here
-  console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+//   console.log("");
 
-  console.log("");
+//   console.log("Expecting: 'ybabtac'");
+//   console.log("=>", reverseString("catbaby"));
+// }
 
-  console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString("catbaby"));
-}
-
-module.exports = reverseString;
+// module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
